13cec2928a3a63a0bc37ae925702f96d300f77a1
hong <dngkgk1052@naver.com>
2024-11-28 14:15:20 +0900

    [AJH] revert... 로 인해서 서선임님 소스 전체 commit

--- chat.py
+++ chat.py
@@ -15,6 +15,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import ssl
 
+import os
 import time
 from datetime import datetime
 import asyncio
@@ -22,6 +23,9 @@ import threading
 from streamlit.components.v1 import html
 
 load_dotenv()
+SENDER_EMAIL_ADDRESS = os.getenv('SENDER_EMAIL_ADDRESS') 
+SENDER_EMAIL_PASSWORD = os.getenv('SENDER_EMAIL_PASSWORD') 
+RECIEVER_EMAIL_ADDRESS = os.getenv('RECIEVER_EMAIL_ADDRESS') 
 
 # 이메일 보내는 함수
 def send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, sender_password):
@@ -65,10 +69,14 @@ def getNextNextMonth():
         next_next_month = (current_year, current_month + 2)
     return f"{next_next_month[0]}년 {next_next_month[1]}월"
 
+def save_logs_in_thread(arr):
+    """로그 저장을 별도의 스레드에서 실행"""
+    asyncio.run(save_question(arr))
+
 st.set_page_config(page_title="A to Z Uracle", page_icon="./images/common/uracle_favicon.png", initial_sidebar_state="collapsed")
 
-# # 메뉴 선택
-# menu = st.sidebar.selectbox("메뉴를 선택해 주세요.", ["Home", "Admin"])
+# 메뉴 선택
+menu = st.sidebar.selectbox("메뉴를 선택해 주세요.", ["Home", "Admin"])
 
 # 각 섹션을 조건에 따라 보여주기
 if menu == "Home":
@@ -160,7 +168,7 @@ if menu == "Home":
                     st.success("저장되었습니다.")
                     st.session_state.recommand_question_list = ai_recommand_questions(st.session_state.prior_info_fm, st.session_state.prior_info_dept, st.session_state.prior_info_pos)
                     st.session_state.recommad_displayed = True
-                    time.sleep(1.5)  # 3초 대기
+                    time.sleep(1.5)  # 1.5초 대기
                     st.rerun()
 
 
@@ -186,7 +194,8 @@ if menu == "Home":
             all_meta_fields_filled = all([st.session_state.prior_info_fm, st.session_state.prior_info_dept, st.session_state.prior_info_pos]) 
 
             if all_meta_fields_filled:
-                save_question([user_question, st.session_state.prior_info_fm, st.session_state.prior_info_dept, st.session_state.prior_info_pos])
+                #save_question([user_question, st.session_state.prior_info_fm, st.session_state.prior_info_dept, st.session_state.prior_info_pos])
+                threading.Thread(target=save_logs_in_thread, args=([user_question, st.session_state.get("prior_info_fm"), st.session_state.get("prior_info_dept"), st.session_state.get("prior_info_pos")],)).start()
             
             ai_response = get_ai_response(user_question) 
             with st.chat_message("ai"):
@@ -226,7 +235,7 @@ if menu == "Home":
     if st.session_state.eform_displayed:
         # 이메일 발송 폼
         with st.form("email_form"):
-            receiver_email = st.text_input("수신자 이메일 주소", value="harryseo0329@uracle.co.kr", disabled=True)
+            receiver_email = st.text_input("수신자 이메일 주소", value=RECIEVER_EMAIL_ADDRESS, disabled=True)
             subject = st.text_input("이메일 제목", value="AtoZ 챗봇의 답변 내용이 부족하여 더 상세한 답변 요청드립니다.")
             last_user_message = st.session_state.message_list[-2]["content"] if len(st.session_state.message_list) > 1 else "질문이 없습니다."
             last_ai_response = st.session_state.message_list[-1]["content"] if len(st.session_state.message_list) > 0 else "답변이 없습니다."
@@ -250,13 +259,10 @@ if menu == "Home":
             submit_button = st.form_submit_button("메일 전송")
 
             if submit_button:
-                # SMTP 서버 설정 (예: Gmail)
-                smtp_server = "smtp.gmail.com"
-                smtp_port = 465  # SSL 포트
                 # 이메일 발송 함수 호출
-                result = send_email("harry0329.developer@gmail.com", receiver_email, subject, body, smtp_server, smtp_port, "deegbyaaffqfsrjz")
+                result = send_email(SENDER_EMAIL_ADDRESS, receiver_email, subject, body, "smtp.gmail.com", 465, SENDER_EMAIL_PASSWORD)
                 st.success(result)
-                time.sleep(3)  # 3초 대기
+                time.sleep(1.5)  # 1.5초 대기
                 st.session_state.ebutton_displayed = False
                 st.session_state.eform_displayed = False
                 st.rerun()
@@ -266,7 +272,7 @@ if menu == "Home":
         # 이메일 발송 폼
         with st.form("resort_email_form"):
             month = getNextNextMonth()
-            receiver_email = st.text_input("수신자 이메일 주소", value="harryseo0329@uracle.co.kr", disabled=True)
+            receiver_email = st.text_input("수신자 이메일 주소", value=RECIEVER_EMAIL_ADDRESS, disabled=True)
             subject = st.text_input("이메일 제목", value=f"속초 및 제주 휴양소 {month} 예약신청합니다.")
             pos = st.radio("휴양소", ["제주", "속초"])
             dept = st.text_input("부서", value="", placeholder="컨버전스개발실")
@@ -299,13 +305,10 @@ if menu == "Home":
                 if not all_fields_filled:
                     st.error("양식을 모두 채워서 작성해주세요.")
                 else:
-                    # SMTP 서버 설정 (예: Gmail)
-                    smtp_server = "smtp.gmail.com"
-                    smtp_port = 465  # SSL 포트
                     # 이메일 발송 함수 호출
-                    result = send_email("harry0329.developer@gmail.com", receiver_email, subject, body, smtp_server, smtp_port, "deegbyaaffqfsrjz")
+                    result = send_email(SENDER_EMAIL_ADDRESS, receiver_email, subject, body, "smtp.gmail.com", 465, SENDER_EMAIL_PASSWORD)
                     st.success(result)
-                    time.sleep(3)  # 3초 대기
+                    time.sleep(1.5)  # 1.5초 대기
                     st.session_state.rbutton_displayed = False
                     st.session_state.rform_displayed = False
                     st.rerun()
@@ -316,36 +319,21 @@ if menu == "Home":
         #st.image(base64.b64decode("data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==".split(",")[1]), caption='참고이미지', use_column_width=True)
 elif menu == "Admin":       
 
-#             # AI 메시지 출력
-#             with st.chat_message("ai"):
-#                 if isinstance(ai_response, str):  # 응답이 문자열일 경우
-#                     st.write(ai_response)
-#                 else:
-#                     for chunk in ai_response:  # 만약 응답이 스트리밍이라면 한 번에 출력
-#                         st.write(chunk)
-#             st.session_state.message_list.append({"role": "ai", "content": ai_response})
-    
-#         #image 불러오기 예시
-#         #st.image(get_image("seo","townhall_ai_20241030103001222.jpg"), caption="참고이미지", use_column_width=True)    
-#         #st.image("https://uracle.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F7e05b6b0-caa6-40af-bb5c-bf5e0c6fb756%2FUntitled.png?table=block&id=94d25cf8-79f7-4a90-a71e-aabfcc3172df&spaceId=d549a58f-3f07-47fc-94e9-076887e1bf1f&width=1420&userId=&cache=v2")
-#         #st.image(base64.b64decode("data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==".split(",")[1]), caption='참고이미지', use_column_width=True)
-# elif menu == "Admin":       
-
-#     password_placeholder = st.empty() 
-#     result_placeholder = st.empty() 
+    password_placeholder = st.empty() 
+    result_placeholder = st.empty() 
   
-#     # 비밀번호 입력 받기
-#     password = password_placeholder.text_input("비밀번호를 입력하세요", type="password")
+    # 비밀번호 입력 받기
+    password = password_placeholder.text_input("비밀번호를 입력하세요", type="password")
  
-#     if password:
-#         if password == "0302":
-#             result_placeholder.success("비밀번호가 맞습니다. 관리 페이지로 이동합니다.")
-#             password_placeholder.empty()
-#             result_placeholder.empty()
-#             admin_page = importlib.import_module("admin")
-#             admin_page.show_page()  # page1.py에서 show_page() 함수를 실행
-#         else:
-#             result_placeholder.error("비밀번호가 틀렸습니다. 다시 시도하세요.")
+    if password:
+        if password == "0302":
+            result_placeholder.success("비밀번호가 맞습니다. 관리 페이지로 이동합니다.")
+            password_placeholder.empty()
+            result_placeholder.empty()
+            admin_page = importlib.import_module("admin")
+            admin_page.show_page()  # page1.py에서 show_page() 함수를 실행
+        else:
+            result_placeholder.error("비밀번호가 틀렸습니다. 다시 시도하세요.")
 
     
 
